be8c94872fcd91a43f546d2850a65a9ab52404d0
shunsaku-sugita <saku.dot@gmail.com>
2024-08-22 22:02:47 -0700

    until chaper 63

---
+++ section5/dist/app.js
@@ -0,0 +1,23 @@
+"use strict";
+class Department {
+    constructor(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    describe() {
+        console.log("Department: " + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+accounting.describe();
+accounting.printEmployeeInformation();
+//# sourceMappingURL=app.js.map
